d0ba60424b594f66cacd554ef1eaeadcdced9c88
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-03-28 12:35:48 -0300

    feat: task04-S1 testa a conexão com banco

--- app/api/test_router.py
+++ app/api/test_router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.core.database import get_db
 
+from app.core.database import get_db
 
 router = APIRouter()
 
+
 @router.get("/test-db")
 def test_db(db: Session = Depends(get_db)):
-    return {'message': "DB conectado com sucesso"}
+    return {"message": "DB conectado com sucesso"}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 
 from app.api import test_router
+from app.core.database import Base, engine
+from app.models.test_table import TestTable
+
 
 app = FastAPI()
 
 app.include_router(test_router.router)
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
